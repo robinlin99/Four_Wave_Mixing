6a8b6845add97f9e85fb71de1bbeccbeaa273f97
Robin Lin <robin.lin@mail.utoronto.ca>
2019-06-19 21:19:17 -0400

    Update simulation_scan_4wave.m

--- simulation_scan_4wave.m
+++ simulation_scan_4wave.m
@@ -1,5 +1,7 @@
 function [] = simulation_scan_4wave(min,max)
 % Scans from minimum to maximum wavelength at a step length of 1nm 
+% Keeping pump wavelength at 1310 nm
+% Need to find the phase-matching condition
 % No output
     for l = min:max
         PIA_Gain(l,20,1310)
