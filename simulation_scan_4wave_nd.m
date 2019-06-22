b22cbe0ee287086aef5e215db1e90de4bd38fda9
Robin Lin <robin.lin@mail.utoronto.ca>
2019-06-22 18:53:26 -0400

    deleted junk

---
+++ simulation_scan_4wave_nd.m
@@ -0,0 +1,10 @@
+function [] = simulation_scan_4wave_nd(min,max)
+% Scans from minimum to maximum wavelength at a step length of 1nm 
+% Keeping pump wavelength at 1310 nm
+% Need to find the phase-matching condition
+% No output
+    for l = min:max
+        PIA_Gain_nd(l,100,1310)
+    end
+end
+
