62d9ed5f58a7b82ee315c2a085164cd015b9dc8b
Robin Lin <robin.lin99@yahoo.com>
2019-08-12 15:10:58 -0400

    n/a

---
+++ Degenerate-FWM/simulation_spm/simulation_spm.m
@@ -0,0 +1,48 @@
+function [] = simulation_spm_degenerate(l)
+    sig_gain1 = 0;
+    sig_gain2 = 0;
+    sig_gain3 = 0;
+    sig_gain4 = 0;
+    sig_gain5 = 0;
+    sig_gain6 = 0;
+    x_vec1 = zeros(1,101);
+    x_vec2 = zeros(1,101);
+    x_vec3 = zeros(1,101);
+    x_vec4 = zeros(1,101);
+    x_vec5 = zeros(1,101);
+    x_vec6 = zeros(1,101);
+    s_gain1 = zeros(1,101);
+    s_gain2 = zeros(1,101);
+    s_gain3 = zeros(1,101);
+    s_gain4 = zeros(1,101);
+    s_gain5 = zeros(1,101);
+    s_gain6 = zeros(1,101);
+    [x_vec1,s_gain1,sig_gain1] = PIA_Gain(l,30,1310);
+    [x_vec2,s_gain2,sig_gain2] = PIA_Gain(l,40,1310);
+    [x_vec3,s_gain3,sig_gain3] = PIA_Gain(l,50,1310);
+    [x_vec4,s_gain4,sig_gain4] = PIA_Gain(l,60,1310);
+    [x_vec5,s_gain5,sig_gain5] = PIA_Gain(l,70,1310);
+    [x_vec6,s_gain6,sig_gain6] = PIA_Gain(l,80,1310);
+
+    % plot for different values of tpa 
+    figure;
+    plot(x_vec1,s_gain1);
+    hold on
+    plot(x_vec2,s_gain2);
+    hold on
+    plot(x_vec3,s_gain3);
+    hold on
+    plot(x_vec4,s_gain4);
+    hold on
+    plot(x_vec5,s_gain5);
+    hold on
+    plot(x_vec6,s_gain6);
+    grid on
+    grid minor
+    xlabel 'Length (mm)';
+    ylabel 'Gain (dB)';
+    legend('Power - 30 mW','Power - 40 mW', 'Power - 50 mW','Power - 60 mW',"Power - 70 mW","Power - 80 mW");
+    fname = '/Users/robinlin/Desktop/Research/2019 Summer/Four_Wave_Mixing/Experiment_scanner_spm_degenerate';
+    fng = sprintf('%0.2f nm Signal Gain degenerate.png',l);
+    saveas(gcf, fullfile(fname,fng));
+end
--- Non-degenerate-FWM/simulation_scan_4wave_4wave.m
+++ Non-degenerate-FWM/simulation_scan_4wave_4wave.m
@@ -20,15 +20,16 @@ function [] = simulation_scan_4wave_4wave(min,max,pump1_l,pump2_l,pump1,pump2)
     
     for l = min:max
         [x_vec1,s_gain1,sig_gain1] = PIA_Gain_4wave(l,pump1, pump2,pump1_l,pump2_l,0);
-        [x_vec2,s_gain2,sig_gain2] = PIA_Gain_4wave(l,pump1, pump2,pump1_l,pump2_l,1);
-        [x_vec3,s_gain3,sig_gain3] = PIA_Gain_4wave(l,pump1, pump2,pump1_l,pump2_l,2);
-        [x_vec4,s_gain4,sig_gain4] = PIA_Gain_4wave(l,pump1, pump2,pump1_l,pump2_l,3);
+        %[x_vec2,s_gain2,sig_gain2] = PIA_Gain_4wave(l,pump1, pump2,pump1_l,pump2_l,1);
+        %[x_vec3,s_gain3,sig_gain3] = PIA_Gain_4wave(l,pump1, pump2,pump1_l,pump2_l,2);
+        %[x_vec4,s_gain4,sig_gain4] = PIA_Gain_4wave(l,pump1, pump2,pump1_l,pump2_l,3);
         sig_gain_arr1(index) = sig_gain1;
-        sig_gain_arr2(index) = sig_gain2;
-        sig_gain_arr3(index) = sig_gain3;
-        sig_gain_arr4(index) = sig_gain4;
+        %sig_gain_arr2(index) = sig_gain2;
+        %sig_gain_arr3(index) = sig_gain3;
+        %sig_gain_arr4(index) = sig_gain4;
         index = index + 1;
         % plot no two photon absorption with two photon absorption on
+        %{
         figure
         plot(x_vec1,s_gain1);
         hold on
@@ -45,9 +46,10 @@ function [] = simulation_scan_4wave_4wave(min,max,pump1_l,pump2_l,pump1,pump2)
         fname = '/Users/robinlin/Desktop/Research/2019 Summer/Four_Wave_Mixing/Experiment_scanner_tpa'
         fng = sprintf('%0.2f nm, Pump1 = %0.2f mW, Pump2 = %0.2f mW Signal Gain.png',l, pump1, pump2);
         saveas(gcf, fullfile(fname,fng));
+        %}
     end
     figure
-    plot(wavelength,sig_gain_arr);
+    plot(wavelength,sig_gain_arr1);
     xlabel 'Wavelength (nm)'
     ylabel 'Gain (dB)'
     grid on
